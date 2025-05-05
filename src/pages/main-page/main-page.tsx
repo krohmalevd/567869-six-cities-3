b5fba5301b7c773b96e5a29c09412ffc60c7425e
Danila Krohmalev <danila31rusk@ya.ru>
2025-05-05 10:40:06 +0300

    feat: Выполнит задание 2.16. Создал и описал компонент главной страницы `main-page`. Создал компонент для отображение списка предложения `cities` и компонент одной карточки предложения в списке `place-card`. Создал компонент `app` для отрисовки главной страницы и `header`. Создал компоненты страниц `favorites-page`, `login-page`, `offer-page`

---
+++ src/pages/main-page/main-page.tsx
@@ -0,0 +1,22 @@
+import Cities from '../../components/cities/cities';
+import Header from '../../components/header/header';
+import Tabs from '../../components/tabs/tabs';
+
+type MainPageProps = {
+    placesCount: number;
+}
+
+function MainPage({placesCount}: MainPageProps): JSX.Element {
+  return (
+    <div className="page page--gray page--main">
+      < Header />
+      <main className="page__main page__main--index">
+        <h1 className="visually-hidden">Cities</h1>
+        < Tabs />
+        < Cities placesCount={placesCount}/>
+      </main>
+    </div>
+  );
+}
+
+export default MainPage;
