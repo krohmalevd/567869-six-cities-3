7c2cc8f9b4f55a1476754f7b8c0d4baa0c7914f7
Danila Krohmalev <danila31rusk@ya.ru>
2025-05-23 15:29:31 +0300

    feat: Добавил маршрутизацию

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,41 @@
+import { AppRoute } from '../../const';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
 
 type AppProps = {
   placesCount: number;
 }
 
-function App({placesCount}: AppProps): JSX.Element {
+function App({ placesCount }: AppProps): JSX.Element {
   return (
-    <MainPage placesCount={placesCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage placesCount={placesCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesPage />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage />}
+        />
+        <Route
+          path='*'
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/const.ts
+++ src/const.ts
@@ -2,3 +2,9 @@ export const Settings = {
   PlacesCount: 5
 };
 
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id'
+}
